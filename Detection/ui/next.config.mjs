5f2cdde462e21a73abab6f3f4616dcdddc8f664b
rushxbh <rushabh9372@gmail.com>
2026-08-09 19:55:26 +0530

    fix Vercel Next.js deployment settings

--- Detection/ui/next.config.mjs
+++ Detection/ui/next.config.mjs
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
-
 const nextConfig = {
-  ...(isGithubActions ? { output: 'export' } : {}),
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
 };
 
 export default nextConfig;
+
